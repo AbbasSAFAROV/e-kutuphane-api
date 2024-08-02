2df4ee763d8159659c67e0529b5e0f4e4013be1b
AbbasSAFAROV <abbasabdullaziz@gmail.com>
2024-08-02 09:31:26 +0300

    guncel

--- src/main/java/com/docuart/library/security/SecurityConfig.java
+++ src/main/java/com/docuart/library/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.docuart.library.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
@@ -18,21 +19,21 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 public class SecurityConfig {
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests((requests) -> requests
-//                        .requestMatchers("/", "/home", "/auth/login", "/auth/login/**").permitAll()
-//                        .anyRequest().authenticated()
-//                )
-//                .logout((logout) -> logout.permitAll());
-//
-//        return http.build();
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/home", "/auth/login", "/auth/login/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
 
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        //http.httpBasic(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
         // Tüm endpointler için doğrulamaya gerek yok
         http.authorizeHttpRequests(c -> c.anyRequest().permitAll());
         return http.build();
